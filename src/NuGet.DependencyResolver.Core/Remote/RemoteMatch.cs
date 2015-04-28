792463b08d6ae65f2e054ef271ddfc41c9a770b6
anurse <andrew@andrewnurse.net>
2015-04-28 12:27:03 -0700

    refactored RestoreCommand a bit

--- src/NuGet.DependencyResolver.Core/Remote/RemoteMatch.cs
+++ src/NuGet.DependencyResolver.Core/Remote/RemoteMatch.cs
@@ -4,10 +4,25 @@ using NuGet.LibraryModel;
 
 namespace NuGet.DependencyResolver
 {
-    public class RemoteMatch
+    public class RemoteMatch : IEquatable<RemoteMatch>
     {
         public IRemoteDependencyProvider Provider { get; set; }
         public LibraryIdentity Library { get; set; }
         public string Path { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as RemoteMatch);
+        }
+
+        public bool Equals(RemoteMatch other)
+        {
+            return other != null && Library.Equals(other.Library);
+        }
+
+        public override int GetHashCode()
+        {
+            return Library.GetHashCode();
+        }
     }
 }
